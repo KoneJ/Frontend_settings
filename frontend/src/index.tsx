c84b1736999a5973bd39ac1ba11d4ffa1b6d827a
KoneJ <kbvv67890@gmail.com>
2023-07-06 15:46:03 +0900

    TS적용 및 파일 확장자 변경

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import 'tailwindcss/tailwind.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+
